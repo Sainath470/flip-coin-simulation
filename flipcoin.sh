1e1af05e41d19a0622588be76dc588130456ff64
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-02 16:55:44 +0530

    Added:second computation

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,10 +1,25 @@
-#!/bin/bash -x
-
+#!/bin/bash 
+declare -A result
+num=20
+while [ $num -gt 0 ]
+do
+((num--))
 flipcoin=$((RANDOM%2))
-
 if [ $flipcoin -eq 1 ]
 then
 	echo " It's Heads "
+	result1=$((result1+1))
+	a[result]=$((result+1))
+	((result++))
 else
 	echo " It's Tails "
+	result2=$((result2+1))
+	b[result]=$((result+1))
+	((result++))
 fi
+done
+echo "head wins" ${a[@]}
+echo "Total head wins" $result1
+echo
+echo "Tail wins" ${b[@]}
+echo "Total Tail wins" $result2
